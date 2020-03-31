e239bafe0e44327f46cd80e11fbbf59c2d7075ce
Shaked <shaked@7sjOF9KU9h.lan>
2020-03-31 12:39:58 +0300

    public

---
+++ backend/public/precache-manifest.d4655acc3131dbbce8cc8fa4badace11.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bec8d4af1ab2d95fcfe5",
+    "url": "/css/app.b4e7e2da.css"
+  },
+  {
+    "revision": "2155508e9b7c4317141d6f6aa9c5212d",
+    "url": "/img/addPost.2155508e.png"
+  },
+  {
+    "revision": "d49e96c8d95161df6957eae5438be715",
+    "url": "/img/add_btn.d49e96c8.png"
+  },
+  {
+    "revision": "1052f748f856587d0aee37358975e656",
+    "url": "/img/login-background.1052f748.png"
+  },
+  {
+    "revision": "3fa679b1ba5565d881b97a7b1b70cc42",
+    "url": "/img/logo.3fa679b1.png"
+  },
+  {
+    "revision": "afa1a3335a89b50c36679d2f94e7b15f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bec8d4af1ab2d95fcfe5",
+    "url": "/js/app.e1ddf350.js"
+  },
+  {
+    "revision": "6e1f571112635dd0848e",
+    "url": "/js/chunk-vendors.a0e0ae78.js"
+  },
+  {
+    "revision": "47ca0b75fa643d43a739580f6d60f77d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "93cb84f6058c9a00f065c5dc14f8789a",
+    "url": "/static/favicon.png"
+  },
+  {
+    "revision": "93cb84f6058c9a00f065c5dc14f8789a",
+    "url": "/static/logo.png"
+  }
+]);
